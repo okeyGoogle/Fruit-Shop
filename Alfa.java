1a6c8aad94d458d3665248ff0eee8e9ba48d39c6
okeyGoogle <goalkeeper_94@mail.ru>
2017-05-10 03:06:08 +0600

    Create Alfa.java

---
+++ Alfa.java
@@ -0,0 +1,10 @@
+public class Alfa {
+public static void main (String[] args) { 
+Boolean flag = false;
+for (ing i=a; i<1000; i++) { char let= (char) i;
+if (let == 'a' || let == 'A') 
+{ 
+flag = true;
+};
+}
+}
